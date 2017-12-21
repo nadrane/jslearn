412857d72f94522976b66a1ba73181e9dc53c3cd
David Machajewski <machajew@gmail.com>
2017-12-21 08:54:35 -0600

    ch 6 reading

---
+++ ch6.js
@@ -0,0 +1,31 @@
+/*
+* Chapter 6 - The Secret Life of Objects - Reading
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+* Chapter 6 - Exercises
+*/
+
+
+
+
+
+
+/*
+* Chapter 6 - Questions
+*/
+// In methods section, don't get apply, bind, call
