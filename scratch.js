602036d3ac7b4554fdbe67daebca9ab46bc4da99
David Machajewski <machajew@gmail.com>
2017-12-25 00:05:02 -0500

    cleanup tools and config data

--- scratch.js
+++ scratch.js
@@ -1,31 +1,14 @@
-function once(f) {
-    var state = 0;
-    return function() {
-        state++;
-        if (state === 0) {
-            return f.apply(null, [].prototype.slice.call(arguments);
-        }
-    };
+const journal = [];
+const map = {};
+
+function once() {
+  return true;
 }
 
-once(function() {
-    console.log('nick')
-})
-
-const decoratedFunc1 = once()
-const decoratedFunc2 = once()
-decoratedFunc1(arg1,... arg10) // nick
-decoratedFunc2()
-
-once(f)()
-once(f)()
-
-console.log('nick')
-
-//Homework
+// Homework
 
 // look up lodash, go through and implement lodash function (map, function, reuce, filter, etc)
-////// specific ones = look at sort, ** look for things that have a predicate
+// specific ones - look at sort,look for things that have a predicate
 // always use const, look at links re: scope of let/const/var
 
 // when get bored, implement all recursively
@@ -38,5 +21,33 @@ console.log('nick')
 
 // then eloquent through ch 15
 
-// solve game of life in 2 ways - 1 is storing state in dom itself (data attirbutes), then storing state in javascript
+// solve game of life in 2 ways - 1 is storing state in dom itself (data attirbutes), & storing state in javascript
 
+{
+  "name": "eloquent",
+  "version": "1.0.0",
+  "description": "",
+  "main": "ancestry.js",
+  "scripts": {
+    "start": "node index.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/machajew/eloquent.git"
+  },
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/machajew/eloquent/issues"
+  },
+  "homepage": "https://github.com/machajew/eloquent#readme",
+  "dependencies": {
+    "express": "^4.16.2",
+    "lodash": "^4.17.4"
+  },
+  "devDependencies": {
+    "eslint-config-airbnb-base": "^12.1.0",
+    "eslint-plugin-import": "^2.8.0"
+  }
+}
