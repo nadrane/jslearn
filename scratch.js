8d6dade1370200b99814190c9f674a5ef89701b4
David Machajewski <machajew@gmail.com>
2017-12-25 00:06:23 -0500

    cleanup pt 2

--- scratch.js
+++ scratch.js
@@ -22,32 +22,3 @@ function once() {
 // then eloquent through ch 15
 
 // solve game of life in 2 ways - 1 is storing state in dom itself (data attirbutes), & storing state in javascript
-
-{
-  "name": "eloquent",
-  "version": "1.0.0",
-  "description": "",
-  "main": "ancestry.js",
-  "scripts": {
-    "start": "node index.js",
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "repository": {
-    "type": "git",
-    "url": "git+https://github.com/machajew/eloquent.git"
-  },
-  "author": "",
-  "license": "ISC",
-  "bugs": {
-    "url": "https://github.com/machajew/eloquent/issues"
-  },
-  "homepage": "https://github.com/machajew/eloquent#readme",
-  "dependencies": {
-    "express": "^4.16.2",
-    "lodash": "^4.17.4"
-  },
-  "devDependencies": {
-    "eslint-config-airbnb-base": "^12.1.0",
-    "eslint-plugin-import": "^2.8.0"
-  }
-}
