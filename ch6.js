19e3bd203ed48cfa665c0cd5fc650aa1d9018d48
David Machajewski <machajew@gmail.com>
2017-12-30 15:24:59 -0500

    completed ch6 reading programs

--- ch6.js
+++ ch6.js
@@ -10,7 +10,7 @@ const MOUNTAINS = [
   { name: 'Mont Blanc', height: 4808, country: 'Italy/France' },
   { name: 'Vaalserberg', height: 323, country: 'Netherlands' },
   { name: 'Denali', height: 6168, country: 'United States' },
-  { name: 'Popocatepetl', height: 5465, country: 'Mexico' }
+  { name: 'Popocatepetl', height: 5465, country: 'Mexico' },
 ];
 
 // TextCell construction, prototype, and helpers
