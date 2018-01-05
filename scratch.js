e5c987a02456490c6be4cc968b324927f771c848
David Machajewski <machajew@gmail.com>
2018-01-05 17:20:18 -0600

    work comp

--- scratch.js
+++ scratch.js
@@ -1,3 +1,4 @@
+/* eslint no-console: "off" */
 // function once(f) {
 //   let state = 0;
 //   const x = function x(...theArgs) {
@@ -10,40 +11,68 @@
 //   return x;
 // }
 
-/* eslint no-console: "off" */
+// function once(f) {
+//   let state = 0;
+//   return (...theArgs) => {
+//     state += 1;
+//     if (state === 1) {
+//       return f(...theArgs);
+//     }
+//     return `this was the ${state} call!`;
+//   };
+// }
 
-function once(f) {
-  let state = 0;
-  return (...theArgs) => {
-    state += 1;
-    if (state === 1) {
-      return f(...theArgs);
-    }
-    return `this was the ${state} call!`;
-  };
+// function argBark(...theArgs) {
+//   for (let i = 0; i < theArgs.length; i += 1) {
+//     console.log(theArgs[i]);
+//   }
+// }
+
+// const dec1 = once(argBark);
+// dec1('hi');
+
+// const ancestry = {};
+
+// // built-in method
+// ancestry.filter(person => person.born > 1900 && person.born < 1925);
+
+
+// ancestry.filter(person => person.father === 'Carel Haverbeke');
+
+// const byName = {};
+
+// const differences = ancestry
+//   .filter(person => person.mother in byName)
+//   .map(person => person.born - byName[person.mother].born);
+
+// console.log(differences);
+
+
+// function foo() {
+//   console.log('hey');
+//   if (!foo.count) {
+//     foo.count = 0;
+//   }
+//   foo.count += 1; // `foo` refers to itself
+// }
+
+function foo() {
+	console.log( this.a );
 }
 
-function argBark(...theArgs) {
-  for (let i = 0; i < theArgs.length; i += 1) {
-    console.log(theArgs[i]);
-  }
+function doFoo(fn) {
+	// `fn` is just another reference to `foo`
+
+	fn(); // <-- call-site!
 }
 
-const dec1 = once(argBark);
-dec1('hi');
+var obj = {
+	a: 2,
+	foo: function() {
+    console.log( this.a );
+  },
+};
 
-const ancestry = {};
+var a = "oops, global"; // `a` also property on global object
 
-// built-in method
-ancestry.filter(person => person.born > 1900 && person.born < 1925);
-
-
-ancestry.filter(person => person.father === 'Carel Haverbeke');
-
-const byName = {};
-
-const differences = ancestry
-  .filter(person => person.mother in byName)
-  .map(person => person.born - byName[person.mother].born);
-
-console.log(differences);
+doFoo( obj.foo ); // "oops, global"
