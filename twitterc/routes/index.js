3aa06634be2276a8f7c8cf62ac40a7a64c0d2ba4
David Machajewski <machajew@gmail.com>
2018-01-17 22:41:48 -0600

    started work on user views

--- twitterc/routes/index.js
+++ twitterc/routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const router = express.Router();
 const fs = require('fs');
+
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
   fs.readFile('tweets.json', (err, data) => {
     if (err) throw err;
-    res.render('index', { tweets: JSON.parse(data) });
+    res.render('index', { tweets: JSON.parse(data), user: 'foo' });
   });
 });
 
@@ -19,4 +20,4 @@ module.exports = router;
 // what does bin/www do? To what extent/depth level should I be comfortable with configuring that?
 // same for app.js
 // why do we launch in console with DEBUG=twitterc:* npm start ?
-// in this current file, when I read from my tweets.JSON, is that read happening every time a GET request is submitted?
+// in this file, is the read to tweets.JSON happening every time a GET request is submitted?
